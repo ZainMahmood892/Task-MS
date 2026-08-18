96fc1c5d49b459a2cab9db227eda9274ad318d49
ZainMahmood892 <zainmahmood183@gmail.com>
2026-08-18 06:12:01 -0600

    Create resources/views/black-button.blade.php via n8n AI code generator

---
+++ resources/views/black-button.blade.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Black Button</title>
+    <style>
+        .black-button {
+            background-color: black;
+            color: white;
+            border: none;
+            padding: 10px 20px;
+            cursor: pointer;
+            font-size: 16px;
+            border-radius: 4px;
+        }
+
+        .black-button:hover {
+            background-color: #222;
+        }
+    </style>
+</head>
+<body>
+    <button class="black-button">Black Button</button>
+</body>
+</html>
