e7a0fe86860e75dd4dbbf434e4d1ee383b652ca2
ZainMahmood892 <zainmahmood183@gmail.com>
2026-08-18 06:25:04 -0600

    Add route for resources/views/red-heading.blade.php via n8n AI code generator

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/red-heading', function () {
+    return view('red-heading');
+});
